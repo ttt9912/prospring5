eec4b5215fad51d380f1cec187a18074643b1cbe
thts <tschachtli.th@gmail.com>
2018-07-14 21:10:07 +0200

    ch3/ch4

---
+++ ch3-ioc-and-di/src/main/java/ch3/apps/p10_bean_inheritance/InheritanceDemo.java
@@ -0,0 +1,27 @@
+package ch3.apps.p10_bean_inheritance;
+
+
+/*
+ * Bean Inheritance:
+ *  #1: multiple beans that are the same type
+ *  #2: multiple beans that implement a shared interface
+ *
+ *
+ */
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class InheritanceDemo {
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("beanInheritance_context.xml");
+        ctx.refresh();
+
+        Singer parent = (Singer) ctx.getBean("parent");
+        Singer child = (Singer) ctx.getBean("child");
+
+        System.out.println("Parent Singer: " + parent);
+        System.out.println("Child Singer: " + child);
+    }
+}
---
+++ ch3-ioc-and-di/src/main/java/ch3/apps/p10_bean_inheritance/Singer.java
@@ -0,0 +1,30 @@
+package ch3.apps.p10_bean_inheritance;
+
+public class Singer {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(final int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Singer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
