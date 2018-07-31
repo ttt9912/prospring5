fb40cd521ffe0ccd1f755fedd91f44b6bc6b43c1
ttt <tschachtli.th@gmail.com>
2018-07-31 22:00:34 +0200

    ch4

--- ch4-configuration-and-springboot/src/main/java/ch4/p11_groovy_config/Demo.java
+++ ch4-configuration-and-springboot/src/main/java/ch4/p11_groovy_config/Demo.java
@@ -3,18 +3,16 @@ package ch4.p11_groovy_config;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.GenericGroovyApplicationContext;
 
-// Groovy Libary must be added: https://www.jetbrains.com/help/idea/getting-started-with-groovy.html
 class Demo {
 
     /*
      * configure bean definitions and ApplicationContext using Groovy
      *
      * GenericGroovyApplicationContext: create ApplicationContext from Groovy Script (ch4.p11_groovy_config.beansaa.groovyvy)
-     *
      */
     @Test
     void groovy() {
-        GenericGroovyApplicationContext ctx = new GenericGroovyApplicationContext("bean_def.groovy");
+        GenericGroovyApplicationContext ctx = new GenericGroovyApplicationContext("bean_definition.groovy");
 
         final Employee employee = ctx.getBean("employee", Employee.class);
         System.out.println(employee);
