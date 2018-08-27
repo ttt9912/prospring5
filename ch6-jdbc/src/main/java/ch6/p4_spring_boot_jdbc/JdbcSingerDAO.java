be616ba36f1141c4f097ef31a39cbbf4dd352c0b
ttt <tschachtli.th@gmail.com>
2018-08-27 23:23:22 +0200

    ch6

--- ch6-jdbc/src/main/java/ch6/p4_spring_boot_jdbc/JdbcSingerDAO.java
+++ ch6-jdbc/src/main/java/ch6/p4_spring_boot_jdbc/JdbcSingerDAO.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JdbcSingerDAO implements SingerDAO {
+class JdbcSingerDAO implements SingerDAO {
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
